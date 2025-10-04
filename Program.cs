07f63d6eda9ebd684a5bb1ffb3e6f043344aacc9
seif <seifelkomy686@gmail.com>
2025-10-04 11:37:58 +0300

    solving adv03

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Ass03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Q1
+
+            //Queue<int> q = new Queue<int> (new[]{ 1, 2, 3, 4, 5, 6, 7, 8 });
+
+            //Stack<int> s = new Stack<int>();
+
+            //while(q.Count > 0)
+            //{
+            //    s.Push (q.Dequeue ());
+            //}
+            //foreach (var item in s)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+
+
+            #endregion
+
+            #region Q2
+
+            //string s = "[()]{}";
+
+            //Stack<char> ch = new Stack<char>();
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    char c = s[i];
+            //    if (c == '{' || c == '[' || c == '(')
+            //        ch.Push(c);
+
+            //    if (c == '}' && ch.Peek() == '{')
+            //        ch.Pop();
+
+            //    if (c == ']' && ch.Peek() == '[')
+            //        ch.Pop();
+
+            //    if (c == ')' && ch.Peek() == '(')
+            //        ch.Pop();
+            //}
+
+            //if(ch.Count == 0)
+            //    Console.WriteLine("Balanced");
+            //else
+            //    Console.WriteLine("UnBalanced");
+
+
+            #endregion
+        }
+    }
+}
