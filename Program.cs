f9b261501455f02ec6dfd170479a8c3c0f62a7aa
seif <seifelkomy686@gmail.com>
2025-10-04 11:30:05 +0300

    solving advanced01

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Advanced01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            /* if the array not sorted so there is at least one swap will happen in the second loop
+            /* if not so the array is sorted and we dont need to continue
+            */
+
+            //int[] arr = { 7, 2, 6, 8, 1, 9, 22, 66, 25, 11, 34 };
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    bool swaped = false;
+            //    for (int j = i + 1; j < arr.Length; j++)
+            //    {
+            //        if (arr[i] > arr[j])
+            //        {
+            //            int tmp = arr[i];
+            //            arr[i] = arr[j];
+            //            arr[j] = tmp;
+            //            swaped = true;
+            //        }
+            //    }
+            //    if (!swaped)
+            //        break;
+
+            //}
+
+            //foreach (var item in arr)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region Q2
+
+            var r1 = new Range<int>(1,100);
+
+            if(r1.IsInRange(50))
+                Console.WriteLine("in range");
+            else
+                Console.WriteLine("out of range");
+
+            Console.WriteLine(r1);
+            Console.WriteLine($"Length of r1 is {r1.LengthOfRange()}");
+
+
+
+            #endregion
+        }
+
+    }
+}
---
+++ Range.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced01
+{
+    internal class Range<T> where T : IComparable<T> , INumber<T>
+    {
+
+        public T Minimum { get; set; }
+        public T Maximum { get; set; }
+        public Range(T minimum, T maximum)
+        {
+            Minimum = minimum;
+            Maximum = maximum;
+        }
+
+        public bool IsInRange(T? value)
+        {
+            if(value == null) return false;
+
+            return value?.CompareTo(Minimum) >= 0 && value?.CompareTo(Maximum) <= 0? true : false; 
+        }
+
+        public T LengthOfRange()
+        {
+            return Maximum - Minimum;
+        }
+
+        public override string ToString()
+        {
+            return $"Range from {Minimum} to {Maximum}";
+        }
+
+
+    }
+}
