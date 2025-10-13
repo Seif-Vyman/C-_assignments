65b5bd8e80a5e6e8d025704185cb316d9b0fe970
seif <seifelkomy686@gmail.com>
2025-10-13 19:25:16 +0300

    adding assignment mvc03

---
+++ Demo.Pl/Program.cs
@@ -0,0 +1,61 @@
+using Demo.BLL.Services;
+using Demo.DAL.Data.Contexts;
+using Demo.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Pl
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Configuration Services : Add services to the DI Container
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //Register Services And Give CLR The Permission To Inject This Service If Needed
+            //builder.Services.AddScoped<ApplicationDbContext>(); 
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+
+                //var conString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+                //var conString = builder.Configuration.GetSection("ConnectionStrings")["DefaultConnection"];
+                var conString = builder.Configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(conString);
+            });
+
+            builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+
+
+            #endregion
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
